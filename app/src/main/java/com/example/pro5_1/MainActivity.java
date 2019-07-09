88b6adfbbc85cd81c1a26f7351f86005b7678f2c
leean_arc <tywinlee1017@naver.com>
2019-07-09 16:11:30 +0900

    5_1 sources

--- app/src/main/java/com/example/pro5_1/MainActivity.java
+++ app/src/main/java/com/example/pro5_1/MainActivity.java
@@ -2,13 +2,45 @@ package com.example.pro5_1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.ex5_05);
+    //        setContentView(R.layout.ex5_05);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT);
+        LinearLayout baseLayout = new LinearLayout(this);
+        baseLayout.setOrientation(LinearLayout.VERTICAL);
+        setContentView(baseLayout,params);
+
+        final EditText edit = new EditText(this);
+        baseLayout.addView(edit);
+
+        Button btn = new Button(this);
+        btn.setText("버튼");
+
+        baseLayout.addView(btn);
+        final TextView t1 = new TextView(this);
+        baseLayout.addView(t1);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                t1.setText(edit.getText());
+            }
+        });
+
+
     }
 }
